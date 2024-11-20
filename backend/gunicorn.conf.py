5525e3c08d25a631701ee46e767db8c53b60daa9
Breno Pelegrin <breno.pelegrin@usp.br>
2024-11-20 00:42:33 -0300

    ADD: backend Dockerfile, upd requirements

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -3,5 +3,5 @@ bind = '[::]:8080'
 accesslog = 'gunicorn.log'
 errorlog = 'gunicorn.error.log'
 loglevel='info'
-workers = int(os.getenv('GUNICORN_WORKERS'))
-threads = int(os.getenv('GUNICORN_THREADS'))
+workers = int(os.getenv('GUNICORN_WORKERS', 1))
+threads = int(os.getenv('GUNICORN_THREADS', 1))
